e27cb9ceeab1b2a8978b85bf30f90a70f8b291b0
Hsifeulbhsifder <zaeemkmohamed@gmail.com>
2016-10-07 12:15:23 -0400

    Finished lego code

--- nxtTwoAxis.c
+++ nxtTwoAxis.c
@@ -16,10 +16,12 @@ task main(){
 		sqrt(sq(width - start.x) + sq(height - start.y))
 	};
 	v2 dests[numDest];
+	displayString(0, "(%d, %d)", start.x, start.y);
 
 	for(int i = 0; i < numDest; i++){
 		dests[i].x = (float) (random(89));
 		dests[i].y = (float) (random(80));
+		displayString(i+1, "(%d, %d)", dests[i].x, dests[i].y);
 
 		nMotorEncoder[motorA] = 0;
 		nMotorEncoder[motorC] = 0;
@@ -38,10 +40,17 @@ task main(){
 		int enc1 = 180.f/(PI * radius) * dist1;
 		int enc2 = 180.f/(PI * radius) * dist2;
 
-		motor[motorA] = 50;
-		while(nMotorEncoder[motorA] < (enc1 - 5) || nMotorEncoder){
-			
-		}
+		motor[motorA] = 50 * (int)(abs(dist1)/dist1);
+		while(nMotorEncoder[motorA] < (enc1 - 5) ||
+			  nMotorEncoder[motorA] > (enc1 + 5)){}
+		motor[motorA] = 0;
+
+		motor[motorC] = 50 * (int)(abs(dist2)/dist2);
+		while(nMotorEncoder[motorC] < (enc2 - 5) ||
+			  nMotorEncoder[motorC] > (enc2 + 5)){}
+		motor[motorC] = 0;
+
+		wait1Msec(4000);
 	}
 
 }
